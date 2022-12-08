3c4bad2bfa10a86d2232ba1e2c7ae0799fa42b79
Anre Eybers <anreinfo@gmail.com>
2022-12-08 18:22:12 +0200

    COD-29: Code review feedback

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -8,7 +8,7 @@ const ChatInput = () => {
   const channelName = "#general";
 
   return (
-    <div className="flex items-center gap-4 rounded-lg bg-discord-input px-4 py-[10px] text-discord-white">
+    <div className="flex items-center gap-4 rounded-lg bg-discord-input px-4 py-2.5 text-discord-white">
       <div className="flex">
         <button type="button" className="group">
           <PlusCircleIcon className="h-6 w-6 opacity-70 group-hover:opacity-100" />
